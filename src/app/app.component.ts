aeef6740f1eb08d2452d3c57cf49cc76120cba41
mateus wesley <mateuswesley314@gmail.com>
2023-03-10 10:36:04 -0300

    Promises no Angular 1- Objetos que tem como objetivo representar um valor de um certo tipo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'observables';
+
+  // Promisse é o objeto que recebe uma função
+  // essa função interna, recebe outras duas funções, resolve e reject
+  // e precisa determinar quando cada uma das duas será executada.
+  
+  // as função resolve e reject recebe retornam uma promise
+  // do tipo Promise<T>, logo devem receber um parámetro do tipo T.
+
+  
+  myPromise(nome: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+
+      // é bom notar que somente uma das duas serao executadas, mesmo 
+      // sem o if
+
+      
+      if(nome === 'mateus'){
+        resolve('nome legal parabens po')
+      }
+
+      else {
+        reject('nome de merda')
+      }
+
+    })
+  }
+
+  ngOnInit(): void {
+
+    // estamos dizendo o que fazer quando o resultado chegar
+    //  a promisse representa uma string, entao estamos fazendo
+    // codigo do que faremos com essa string, quando tivermos esse valor
+    // nesse caso é so imprimir ela na tela
+    this.myPromise('ok')
+      .then((value) => {console.log(value)})
+      .catch(erro => {console.log(erro)})
+
+
+  }
 }
+
